279e8f418bfc050de0ecba3ef6081e97178c3221
hz <13457698+hozzjss@users.noreply.github.com>
2023-11-05 02:55:03 +0200

    Add server close event and stop scheduler

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,13 @@ import { config as loadEnvVars } from "dotenv";
 import { createServer } from "express-zod-api";
 import { routing } from "./router/router";
 import { config as serverConfig } from "./server-config";
+import { toadScheduler } from "./scheduler/toad";
 
 loadEnvVars();
 
 const { app, logger } = createServer(serverConfig, routing);
 export { app, logger };
+app.on("close", () => {
+    logger.info("Closing server");
+    toadScheduler.stop();
+});
